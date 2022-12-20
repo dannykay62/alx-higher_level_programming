4a7e79d85e0ca190f63323559ab902eccb80a1d7
dannykay62 <danielkay62@gmail.com>
2022-12-20 14:40:43 -0800

    a class Square that defines a square by: (based on 5-square.py)
    Private instance attribute: size:
    property def size(self): to retrieve it
    property setter def size(self, value): to set it:
    size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
    if size is less than 0, raise a ValueError exception with the message size must be >= 0
    Private instance attribute: position:
    property def position(self): to retrieve it
    property setter def position(self, value): to set it:
    position must be a tuple of 2 positive integers, otherwise raise a TypeError exception with the message position must be a tuple of 2 positive integers
    Instantiation with optional size and optional position: def __init__(self, size=0, position=(0, 0)):
    Public instance method: def area(self): that returns the current square area
    Public instance method: def my_print(self): that prints in stdout the square with the character #:
    if size is equal to 0, print an empty line
    position should be use by using space - Don’t fill lines by spaces when position[1] > 0
    You are not allowed to import any module

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,13 +6,11 @@
 """Define a class Square."""
 
 
-
 class Square:
     """Represent a square."""
 
     def __init__(self, size = 0, position = (0, 0)):
         """Initialize a new square.
-
         Args:
             size (size): The size of the new square.
             position (int, int): The position of the new square.
