4aa00b3b2bad3f373cd4ed942de5ea58ca7948a0
dannykay62 <danielkay62@gmail.com>
2022-12-20 05:02:25 -0800

    An empty class Square that defines a square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""Class Square"""
+class Square:
+    """empty class Square that defines a square"""
+
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,5 @@
+# 0x06. Python - Classes and Objects
+
+# Learning Objects and Classes in python
+
+# Programming is fun!!!
