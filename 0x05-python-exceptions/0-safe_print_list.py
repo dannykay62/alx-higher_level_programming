7e216c4e27792600ca62e24d26a84ebf0d6d491f
dannykay62 <danielkay62@gmail.com>
2022-12-19 19:38:04 -0800

    Fixed errors in file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -17,6 +17,5 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
                     ret += 1
         except IndexError:
-            break
-    print("")
+            print("")
     return (ret)
