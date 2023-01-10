b9d25650c7e4e8f7ff606eaf194591b254ed7866
dannykay62 <danielkay62@gmail.com>
2023-01-09 17:11:49 -0800

    Some corrections made

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module defines is_kind_of_class function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """returns True if isinstance or False if not"""
+    return isinstance(obj, a_class)
