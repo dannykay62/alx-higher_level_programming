e49d61ab627253914c0ad44d4d8c8fae56ae3616
dannykay62 <danielkay62@gmail.com>
2023-02-16 16:44:10 -0800

    a script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server.

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- script that lists the number of records with the same score in the table second_table
 -- of the database hbtn_0c_0 in your MySQL server.
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
