8b564a69707c5c9d437b9d89dedeba0ef437bffa
dannykay62 <danielkay62@gmail.com>
2023-02-17 02:28:36 -0800

    a script that creates the database hbtn_0d_2 and the user user_0d_2.

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
 -- script that creates the database hbtn_0d_2 and the user user_0d_2.
-
 -- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
 -- The user_0d_2 password should be set to user_0d_2_pwd
 -- If the database hbtn_0d_2 already exists, your script should not fail
