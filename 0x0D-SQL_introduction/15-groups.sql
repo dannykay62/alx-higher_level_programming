233d6700a743a5fe61ca8d34c3831d9e5f3db5aa
dannykay62 <danielkay62@gmail.com>
2023-02-16 16:33:09 -0800

    script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,3 @@
+-- script that lists the number of records with the same score in the table second_table
+-- of the database hbtn_0c_0 in your MySQL server.
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
