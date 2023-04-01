dfdca3572cd6edb2f24f35253e655d9274976115
dannykay62 <danielkay62@gmail.com>
2023-03-31 18:47:57 -0700

    a Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Takes in a URL, sends a request to the URL and displays the body of
+the response (decoded in utf-8)
+
+Usage: ./3-error_code.py <URL>
+    - Handles HTTP errors.
+"""
+import sys
+import urllib.request
+import urllib.error
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+        except urllib.error.HTTPError as e:
+            print("Error code: {}".format(e.code))
